22687025d21b1f2e33fa40b06ecbc9da7a9e56b6
Temur Chitashvili <temur.chitashvilii@gmail.com>
2024-08-29 22:19:19 +0400

    add Request functions using  Async/Await and URLSession

---
+++ NetworkingPackage/Sources/NetworkingPackage/Networkable.swift
@@ -0,0 +1,99 @@
+//
+//  File.swift
+//
+//
+//  Created by Temur Chitashvili on 29.08.24.
+//
+
+import Foundation
+import Combine
+
+protocol Networkable {
+    @available(iOS 13.0.0, *)
+    func sendRequest<T: Decodable>(endPoint: EndPoint) async throws -> T
+    func sendRequest<T: Decodable>(endPoint: EndPoint, resultHandler: @escaping (Result <T, NetworkError>) -> Void)
+}
+
+final class NetworkService: Networkable {
+    static let shared = NetworkService()
+    
+    private init() { }
+    
+    func sendRequest<T: Decodable>(endPoint endpoint: EndPoint, resultHandler: @escaping (Result<T, NetworkError>) -> Void) {
+        
+        guard let urlRequest = createRequest(endPoint: endpoint) else {
+            return
+        }
+        let urlTask = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard error == nil else {
+                resultHandler(.failure(.invalidURL))
+                return
+            }
+            guard let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode else {
+                resultHandler(.failure(.unexpectedStatusCode))
+                return
+            }
+            guard let data = data else {
+                resultHandler(.failure(.unknown))
+                return
+            }
+            guard let decodedResponse = try? JSONDecoder().decode(T.self, from: data) else {
+                resultHandler(.failure(.decode))
+                return
+            }
+            resultHandler(.success(decodedResponse))
+        }
+        urlTask.resume()
+    }
+    
+    @available(iOS 13.0.0, *)
+    func sendRequest<T: Decodable>(endPoint endpoint: EndPoint) async throws -> T {
+        guard let urlRequest = createRequest(endPoint: endpoint) else {
+            throw NetworkError.decode
+        }
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
+                .dataTask(with: urlRequest) { data, response, _ in
+                    guard response is HTTPURLResponse else {
+                        continuation.resume(throwing: NetworkError.invalidURL)
+                        return
+                    }
+                    guard let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode else {
+                        continuation.resume(throwing:
+                                                NetworkError.unexpectedStatusCode)
+                        return
+                    }
+                    guard let data = data else {
+                        continuation.resume(throwing: NetworkError.unknown)
+                        return
+                    }
+                    guard let decodedResponse = try? JSONDecoder().decode(T.self, from: data) else {
+                        continuation.resume(throwing: NetworkError.decode)
+                        return
+                    }
+                    continuation.resume(returning: decodedResponse)
+                }
+            task.resume()
+        }
+    }
+}
+
+extension Networkable {
+    fileprivate func createRequest(endPoint: EndPoint) -> URLRequest? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = endPoint.scheme
+        urlComponents.host = endPoint.host
+        urlComponents.path = endPoint.path
+        guard let url = urlComponents.url else {
+            return nil
+        }
+        let encoder = JSONEncoder()
+        var request = URLRequest(url: url)
+        request.httpMethod = endPoint.method.rawValue
+        request.allHTTPHeaderFields = endPoint.header
+        if let body = endPoint.body {
+            request.httpBody = try? encoder.encode(body)
+        }
+        return request
+    }
+}
---
+++ NetworkingPackage/Sources/NetworkingPackage/RequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Temur Chitashvili on 29.08.24.
+//
+
+import Foundation
+
+public enum RequestMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case PATCH = "PATCH"
+    case delete = "DELETE"
+}
